7c15c18a51bac819e0714ad9f225b36f621c07ce
Lex Galante <alex_gm_100@hotmail.com>
2020-01-24 00:32:02 -0300

    *

---
+++ Dotnet.WCF.Client/Dotnet.WCF.Client.Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dotnet.WCF.Client.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
